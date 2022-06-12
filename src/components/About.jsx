cd57e5a9e37221328f11fdda07a2df7c2c177faa
lotlouischoitslab <louis.sw.cho@gmail.com>
2022-06-12 14:11:48 -0500

    Finished Website

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,8 +7,8 @@ const About = () => {
     <div name="about" className="aboutme w-full h-screen bg-black text-white p-8">
         <div className="flex flex-col justify-center items-center w-full h-full">
             <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
-                <div className="sm:text-left pb-8 pl-4">
-                    <p className="text-2xl font-bold inline border-b-4 border-white">ABOUT ME</p>
+                <div className="sm:text-left pb-8 justify-left">
+                    <p className="text-2xl font-bold inline border-b-4 border-[white]">ABOUT ME</p>
                 </div>
           
                 </div>
