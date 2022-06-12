78dc0d76259e6ac58cd3b0489d2869e8c455254d
lotlouischoitslab <louis.sw.cho@gmail.com>
2022-06-12 01:30:52 -0500

    check again

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
                 </div>
                 <div className="max-w-[1000px] w-auto grid sm:grid-cols-2 gap-8 px-4">
                     <div className="text-2xl sm:text-left pl-100 max-w-[700px]">
-                        <p> I always envision how AI and IoT will change 
+                        <p className='text-2xl sm:text-2xl'> I always envision how AI and IoT will change 
                         the mobility system for smart cities. From deep learning autonomous vehicles to traffic lights controlled by machine learning,
                         mobile computing for CAVS, and real time analysis in vehicle platooning and distributed database systems for traffic data management.
                         Civil and environmental engineers and computer scientists have been given a mega-scale task to enhance our mobility systems for
@@ -22,7 +22,10 @@ const About = () => {
                         </p>
                         
                     </div>
-                    <img className='w-50 h-80 pl-20' src={Louis}/>
+                    <div className="text-2xl sm:text-left pl-100 max-w-[700px]">
+                        <img className='w-100 h-80 pl-20' src={Louis}/>
+                        
+                    </div>     
                 </div>
 
         </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,22 +7,15 @@ const Home = () => {
         {/*Container*/}
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
             <p className= 'text-2xl sm:text-2xl font-bold text-[#ccd6f6]'>Hello. My name is</p>
-            <h1 className='text-3xl sm:text-7xl font-bold text-[#ccd6f6]'>Louis Sungwoo Cho</h1>
-            <h1 className='text-2xl sm:text-5xl font-bold text-[#ccd6f6]'>조성우</h1>
-            <h2 className='text-2xl sm:text-3xl font-bold text-[#ccd6f6]'>I am a Transportation Engineer and a Computer Scientist</h2>
+            <h1 className='text-3xl sm:text-5xl font-bold text-[#ccd6f6]'>Louis Sungwoo Cho</h1>
+            <h1 className='text-2xl sm:text-2xl font-bold text-[#ccd6f6]'>조성우</h1>
+            <h2 className='text-2xl sm:text-2xl font-bold text-[#ccd6f6]'>I am a Transportation Engineer and a Computer Scientist</h2>
             <p className='text-[#ccd6f6] py-4 max-w-[300px]'>I am an aspiring Transportation Engineer and a Computer Scientist
                 enthusiastic about developing Autonomous Vehicles and Intelligent Transportation Systems (ITS) 
                 using Aritificial Intelligence (AI) and Internet of Things (IoT). I have always envisioned how urban mobility would look like in
                 futuristic Smart Cities.
             </p>
-            <div>
-                <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#ccd6f6] hover:border-[#ccd6f6]'>
-                View Work 
-                <span className='group-hover:rotate-90 duration-300'>
-                <HiArrowNarrowRight className='ml-3'/>
-                </span>
-                </button>
-            </div>
+            
         </div>
     </div>
   )
