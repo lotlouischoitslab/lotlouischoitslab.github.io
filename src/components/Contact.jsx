bee784575871b589da00da26cb59d3a59e71aa91
lotlouischoitslab <louis.sw.cho@gmail.com>
2022-06-12 16:54:16 -0500

    go

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -23,13 +23,12 @@ const Contact = () => {
             </a>
 
         </div>
-          <p className='text-2xl text-white'>Please submit the form below or shoot me an email to louis.sw.cho@gmail.com</p>
+          <p className='text-1xl text-white'>Please submit the form below or shoot me an email to louis.sw.cho@gmail.com</p>
         </div>
-
-        <input className= 'my-2 p-2 bg-[white]' type="text" placeholder="Name" name='name' />
-        <input className= 'my-2 p-2 bg-[white]' type="email" placeholder="Email" name='email' />
+        <input className= 'my-1 p-2 bg-[white]' type="text" placeholder="Name" name='name' />
+        <input className= 'my-1 p-2 bg-[white]' type="email" placeholder="Email" name='email' />
         <textarea className= 'bg-[white] p-2' name="message" rows="10" placeholder="Message"></textarea>
-        <button className= 'text-white border-2 hover:bg-white hover:border-white px-4 pb-3 my-2 mx-auto flex items-center'>Let's Collaborate!</button>
+        <button className= 'text-white border-2 hover:bg-white hover:border-white px-4 pb-1 my-2 mx-auto flex items-center'>Let's Collaborate!</button>
       </form>
     </div>
   )
