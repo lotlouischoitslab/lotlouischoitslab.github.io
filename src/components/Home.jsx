93ee321712b3e020367301f04adbdfd37fa3bd3f
lotlouischoitslab <louis.sw.cho@gmail.com>
2022-06-16 20:00:35 -0500

    skills edited

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,44 +3,6 @@ import './css/Home.css'
 import Typist from 'react-typist'
 import FadeIn from 'react-fade-in'
 
-// const Home = () => {
-//   return (
-//     <div name='home' className="homepage">
-//         {/*Container*/}
-//         <div id='intro'>
-          
-//           <Typist>
-//             <span className="title">
-//               {"Hello. I am"}
-//               <span className="name">{"Louis Sungwoo Cho"}</span>
-//             </span>
-//           </Typist>
-//           <FadeIn delay={1000}>
-//             <span className="sub-title">
-//               Transportation Engineer and Computer Scientist
-//             </span>
-//             <div className="bio">
-//                 I am an aspiring Transportation Engineer and a Computer Scientist
-//                 enthusiastic about developing Autonomous Vehicles and Intelligent Transportation Systems (ITS) 
-//                 using Aritificial Intelligence (AI) and Internet of Things (IoT). I have always envisioned how urban mobility would look like in
-//                 futuristic Smart Cities.
-//             </div>
-//           </FadeIn>
-//             {/* <p className= 'text-2xl sm:text-2xl font-bold text-[white]'>Hello. My name is</p>
-//             <h1 className='text-3xl sm:text-5xl font-bold text-[white]'>Louis Sungwoo Cho</h1>
-//             <h1 className='text-2xl sm:text-2xl font-bold text-[white]'>조성우</h1>
-//             <h2 className='text-2xl sm:text-2xl font-bold text-[white]'>I am a Transportation Engineer and a Computer Scientist</h2>
-//             <p className='text-[white] py-4 max-w-full'>I am an aspiring Transportation Engineer and a Computer Scientist
-//                 enthusiastic about developing Autonomous Vehicles and Intelligent Transportation Systems (ITS) 
-//                 using Aritificial Intelligence (AI) and Internet of Things (IoT). I have always envisioned how urban mobility would look like in
-//                 futuristic Smart Cities.
-//             </p> */}
-            
-//         </div>
-//     </div>
-//   )
-// }
-
 class Home extends React.Component {
   render() {
       return (
@@ -54,10 +16,10 @@ class Home extends React.Component {
                   </Typist>
               <FadeIn delay={1000}>
                 <span className="sub-title">
-                    Transportation Engineer and a Computer Scientist.
+                    Computational Transportation Scientist
                 </span>
                 <div className = "bio">
-                  I am an aspiring Transportation Engineer and a Computer Scientist
+                  I am an aspiring Computational Transportation Scientist
                   enthusiastic about developing Autonomous Vehicles and Intelligent Transportation Systems (ITS) 
                   using Aritificial Intelligence (AI) and Internet of Things (IoT). I have always envisioned how urban mobility would look like in
                   futuristic Smart Cities.
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,56 +10,56 @@ import ReactJS from '../assets/react.png'
 
 import './css/Skills.css'
 
-const Skills = () => {
-  return (
-    <div id='skills' name='skills' className='skills w-full h-screen bg-black text-white p-4'>
-      {/*Container */}
-      <div className='max-w-[1000px] mx-auto p-8 flex flex-col justify-center w-full h-full'>
-        <div> 
-          <p className='text-4xl font-bold inline border-b-4 border-[white]'>SKILLS</p>
-          <p className='text-3xl py-4'>CODING IN: </p>
-        </div>
-
-        <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src={Python} alt="Python icon"/>
-            <p className='my-4'>PYTHON</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src={CPP} alt="CPP icon"/>
-            <p className='my-4'>C++</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src={Java} alt="Java icon"/>
-            <p className='my-4'>JAVA</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <a href="https://github.com/lotlouischoitslab?tab=repositories">
-              <img className='w-20 mx-auto' src={Github} alt="GitHub icon"/>
-              <p className='my-4'>GITHUB</p>
-            </a>
+class Skills extends React.Component {
+  render () {
+    return (
+        <div name='skills' id='skillsid' className='skills'>
+            <div className='skills-title'> 
+              SKILLS
+            </div>
+            <div className='code'>
+              CODING IN:
+            </div>
+            <div className='skills-grid'>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={Python} alt="Python icon"/>
+                <p className='my-4'>PYTHON</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={CPP} alt="CPP icon"/>
+                <p className='my-4'>C++</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={Java} alt="Java icon"/>
+                <p className='my-4'>JAVA</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <a href="https://github.com/lotlouischoitslab?tab=repositories">
+                  <img className='w-20 mx-auto' src={Github} alt="GitHub icon"/>
+                  <p className='my-4'>GITHUB</p>
+                </a>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={HTML} alt="HTML icon"/>
+                <p className='my-4'>HTML</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={CSS} alt="CSS icon"/>
+                <p className='my-4'>CSS</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={JavaScript} alt="JavaScript icon"/>
+                <p className='my-4'>JAVASCRIPT</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={ReactJS} alt="REACTJS icon"/>
+                <p className='my-4'>REACT</p>
+              </div>
+            </div>
             
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src={HTML} alt="HTML icon"/>
-            <p className='my-4'>HTML</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src={CSS} alt="CSS icon"/>
-            <p className='my-4'>CSS</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src={JavaScript} alt="JavaScript icon"/>
-            <p className='my-4'>JAVASCRIPT</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <img className='w-20 mx-auto' src={ReactJS} alt="REACTJS icon"/>
-            <p className='my-4'>REACT</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+        </div>  
+      );
+  }
 }
 
 export default Skills
