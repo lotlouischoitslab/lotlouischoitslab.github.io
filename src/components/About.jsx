645903e0882c9d1e4b6cf0d9f22d3b3d326f71a8
lotlouischoitslab <louis.sw.cho@gmail.com>
2022-07-01 12:14:00 -0500

    fixed aboutme

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ class About extends React.Component {
                 <div className="about-grid">
                     <div className="about-bio">
                         As a Computational Transportation Scientist, I always envision how AI and IoT will change 
-                        our mobility system smart cities. From deep learning autonomous vehicles to traffic lights controlled by machine learning algorithms, optimization in transit planning,
+                        our transportation systems. From deep learning autonomous vehicles to traffic lights controlled by machine learning algorithms, optimization in transit planning,
                         mobile computing, wireless communication and real time analysis in vehicle platooning for CAVs, and distributed database systems for traffic data management,
                         civil and environmental engineers and computer scientists have been given a mega-scale task to enhance our mobility systems for
                         smart cities. I am very enthusiastic about ITS and automating our transportation systems to make traveling as efficient as possible.
