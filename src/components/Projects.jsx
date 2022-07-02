26986ffa1259d1f42fb6853dbd90cc504b062dc4
lotlouischoitslab <louis.sw.cho@gmail.com>
2022-07-02 13:55:05 -0500

    added project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import asam from '../assets/opendrive.png'
+import airline from '../assets/a350.png'
 import cumtd from '../assets/cumtd.jpg'
-import ite from '../assets/ite.png'
 import amtrak from '../assets/amtrak.jpg'
+import ite from '../assets/ite.png'
 import web from '../assets/web.png'
 import './css/Projects.css'
 
@@ -37,16 +38,38 @@ class Projects extends React.Component {
                 <a href="https://github.com/lotlouischoitslab/GraphGeneration/tree/louis_simple_map">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>DryVR++ Map Project</button>
                 </a>
+              </div>
+            </div>
+          </div>
+
+
+          <div 
+          style={{ backgroundImage: `url(${airline})`}}
+          className='shadow-lg shadow-black group container rounded-md flex justify-center items-center mx-auto content-div'>            
+            {/*Hover*/}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                
+              </span>
+              <div className='pt-8 text-center'>
+                <a href="https://github.com/lotlouischoitslab/Airline_Satisfaction">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Airline Satisfaction</button>
+                </a>
                 
 
               </div>
             </div>
           </div>
 
+          {/*Grid Item */}
+
+
+
+
+
           <div 
           style={{ backgroundImage: `url(${cumtd})`}}
-          className='shadow-lg shadow-black group container rounded-md flex justify-center items-center mx-auto content-div'>
-            
+          className='shadow-lg shadow-black group container rounded-md flex justify-center items-center mx-auto content-div'>            
             {/*Hover*/}
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
@@ -63,6 +86,8 @@ class Projects extends React.Component {
           </div>
 
           {/*Grid Item */}
+
+
 
           <div 
           style={{ backgroundImage: `url(${amtrak})`}}
