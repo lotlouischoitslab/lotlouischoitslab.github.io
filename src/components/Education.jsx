d0524edd56dc46c25ac54253e1f2d26db45d560d
lotlouischoitslab <louis.sw.cho@gmail.com>
2022-08-16 01:42:49 -0500

    add a;;

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './css/Education.css'
-import EducationTabs from './EducationTabs'
 
 class Education extends React.Component {
   render (){
@@ -9,9 +8,22 @@ class Education extends React.Component {
       <div className='education-title'>
         EDUCATION
       </div>
-      <div className='experience-list'>
-          <EducationTabs/>
-      </div>
+      <div className ="university-title">
+            University of Illinois at Urbana-Champaign <span className = "title-colored"></span>
+          </div>
+          <div className = "education-date">
+            August 2020 - May 2024 (Expected)
+          </div>
+          <ul class='education-description'>
+            <li className='elem'>
+              Pursuing a B.S in Civil & Environmental Engineering (Specialization: Transportation Engineering)
+            </li>
+            <li className='elem'>
+              Pursuing a Minor in Computer Science (Focus Area: Artificial Intelligence & Big Data)
+            </li>
+
+          </ul> 
+      
     </div>
   )
  }
--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import './css/Experiences.css'
-import ExperienceTabs from './ExperienceTabs'
-
 class Experiences extends React.Component {
     render () {
         return (
@@ -9,9 +7,34 @@ class Experiences extends React.Component {
                 <div className='experiences-title'>
                     EXPERIENCES
                 </div>
-                <div className='experience-list'>
-                    <ExperienceTabs/>
+                
+                <div className ="job-title">
+                    Research Assistant @ Reliable Autonomy Group UIUC
                 </div>
+                <div className ="professor">
+                    Advised by Professor Sayan Mitra, Electrical & Computer Engineering @ UIUC
+                </div>
+                <div className = "job-date">
+                    May 2022 - Present
+                </div>
+                <div className='project-name'>
+                    DryVR++ Lane Map Generator
+                </div>
+                <div className = "sub-job-date">
+                    May 2022 - August 2022
+                </div>
+                <ul class='job-description'>
+                    <li className='elem'>
+                    Created parsing function to read road geometry from ASAM Opendrive files.
+                    </li>
+                    <li className='elem'>
+                    Plotted road geometry for data visualization.
+                    </li>
+                    <li className='elem'>
+                    Generated lanes for autonomous driving simulators.
+                    </li>
+
+                </ul> 
             </div>
         )
     }
