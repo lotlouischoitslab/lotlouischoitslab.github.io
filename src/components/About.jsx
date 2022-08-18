1c16190cfc1e36813009fb0a68520624a925b3f8
lotlouischoitslab <louis.sw.cho@gmail.com>
2022-08-18 01:59:10 -0500

    more edits made

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,11 +11,12 @@ class About extends React.Component {
                 </div>
                 <div className="about-grid">
                     <div className="about-bio">
-                        Growing up in South Korea, I have always been fascinated about transportation networks. I enjoy traveling by airplanes, trains and cars.
-                        With AI and IoT enhancing our mobility, my interests in Intelligent Transportation Systems (ITS) are deep learning and computer vision for autonomous vehicles, 
+                        Growing up in South Korea, I have always been fascinated about transportation networks. I enjoy traveling by airplanes, trains and cars and tried various delicious food.
+                        With AI and IoT enhancing our mobility, my interests in Intelligent Transportation Systems (ITS) are motion planning 
+                        algorithms using deep reinforcement learning, computer vision, and perception for autonomous vehicles, 
                         machine learning and data science for analyzing and forecasting traffic trends, optimizing transit planning, mobile computing, wireless communication and 
-                        real time analysis for vehicle platooning.
-                        My hobbies include listening and singing Korean pop and rap songs, eating Korean food, watching Korean TV shows and dramas, and photography.
+                        real time analysis for vehicle platooning. I am fluent in both English and Korean.
+                        My hobbies are listening and singing Korean pop and rap songs, eating Korean food, and watching Korean TV shows and dramas.
                     </div>
                     <div className='about-img'>
                         <img src={Louis}/>
--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -7,7 +7,35 @@ class Experiences extends React.Component {
                 <div className='experiences-title'>
                     EXPERIENCES
                 </div>
+
+                {/*Experience List Startpoint */}
+                <div className ="job-title">
+                    President @ Institute of Transportation Engineers UIUC Chapter
+                </div>
+                <div className ="professor">
+                    Advised by Professor Lewis Lehe, Civil & Environmental Engineering @ UIUC
+                </div>
+                <div className = "job-date">
+                    August 2022 - Present
+                </div>
+                <ul class='job-description'>
+                    <li className='elem'>
+                        Currently in charge of organizing and hosting transportation conferences and seminars
+                    </li>
+                    <li className='elem'>
+                        Connecting prosepective students and networking with transportation professionals
+                    </li>
+                    <li className='elem'>
+                        Contributed to building the website for ITE@Illinois:
+                        <a href="https://ite.cee.illinois.edu/"> Click Here</a>
+                    </li>
+                </ul> 
+                {/*Experience List Endpoint */}
+
+
                 
+
+                {/*Experience List Startpoint */}
                 <div className ="job-title">
                     Research Assistant @ Reliable Autonomy Group UIUC
                 </div>
@@ -18,23 +46,23 @@ class Experiences extends React.Component {
                     May 2022 - Present
                 </div>
                 <div className='project-name'>
-                    DryVR++ Lane Map Generator
+                    DryVR++ Lane Map Generator for Control Verification
                 </div>
-                <div className = "sub-job-date">
-                    May 2022 - August 2022
-                </div>
+            
                 <ul class='job-description'>
                     <li className='elem'>
-                    Created parsing function to read road geometry from ASAM Opendrive files.
+                    Parsed road attributes from ASAM OpenDRIVE files into Python files
                     </li>
                     <li className='elem'>
-                    Plotted road geometry for data visualization.
+                    Created a data visualizing function to draw the road attributes inside the ASAM OpenDRIVE file
                     </li>
                     <li className='elem'>
-                    Generated lanes for autonomous driving simulators.
+                        Generated multiple lanes from the file parser into the control verification simulators
                     </li>
-
                 </ul> 
+                {/*Experience List Endpoint */}
+
+
             </div>
         )
     }
