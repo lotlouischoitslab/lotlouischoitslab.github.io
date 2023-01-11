b091cde961da906ca5c286eea0f4020402317987
lotlouischoitslab <louis.s.cho@gmail.com>
2023-01-10 18:18:58 -0600

    add

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -37,7 +37,7 @@ class Projects extends React.Component {
                 </span>
                 <div className='pt-8 text-center'>
                   <a href="https://github.com/lotlouischoitslab/Transportation_Engineering">
-                    <button className='text-center rounded-lg px-4 py-3 m-5 bg-white text-gray-700 font-bold text-lg'>Computational Transportation Science Open Source</button>
+                    <button className='text-center rounded-lg px-4 py-3 m-5 bg-white text-gray-700 font-bold text-lg'>Computational Transportation Science (CTS) Open Source</button>
                   </a>
                   
 
