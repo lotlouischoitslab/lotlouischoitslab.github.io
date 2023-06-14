7cbb7fc22d66091b73a13620307bc09cf8c14a22
lotlouischoitslab <louis.s.cho@gmail.com>
2023-06-14 14:43:50 -0500

    add

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,45 +16,45 @@ const Contact = () => {
           CONTACT
       </div>
       <div className="contact-grid">
-        <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500 inline-block'>
+        <div className="social-link">
             <a href="https://www.linkedin.com/in/louis-sungwoo-cho-978502194/">
-              <img className='w-20 mx-auto' src={Linkedin} alt="LinkedIn icon"/>
-              <p className='socials'>LinkedIn</p>
+              <img className="social-link-image" src={Linkedin} alt="Linkedin icon" />
+              <p className="social-link-text">LinkedIn</p>
             </a>
           </div>
 
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500 inline-block'>
+          <div className="social-link">
             <a href="https://github.com/lotlouischoitslab">
-              <img className='w-20 mx-auto' src={Github} alt="GitHub icon"/>
-              <p className='socials'>GITHUB</p>
+              <img className="social-link-image" src={Github} alt="GitHub icon" />
+              <p className="social-link-text">GITHUB</p>
             </a>
           </div>
 
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500 inline-block'>
+          <div className="social-link">
             <a href= {CV}>
-              <img className='w-20 mx-auto' src={CV_img} alt="CV icon"/>
-              <p className='socials'>CV</p>
+              <img className="social-link-image" src={CV_img} alt="CV icon"/>
+              <p className="social-link-text">CV</p>
             </a>
           </div>
 
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500 inline-block'>
+          <div className="social-link">
             <a href= 'https://www.instagram.com/lotlouischolabs/'>
-              <img className='w-20 mx-auto' src={Insta} alt="Instagram icon"/>
-              <p className='socials'>Instagram</p>
+              <img className="social-link-image" src={Insta} alt="Instagram icon"/>
+              <p className="social-link-text">Instagram</p>
             </a>
           </div>
 
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500 inline-block'>
+          <div className="social-link">
             <a href= 'https://discord.com/channels/@lotlouischo#8034'>
-              <img className='w-20 mx-auto' src={Discord} alt="Discord icon"/>
-              <p className='socials'>Discord</p>
+              <img className="social-link-image" src={Discord} alt="Discord icon"/>
+              <p className="social-link-text">Discord</p>
             </a>
           </div>
 
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500 inline-block'>
+          <div className="social-link">
             <a href= 'https://www.facebook.com/louis.sw.cho'>
-              <img className='w-20 mx-auto' src={Meta} alt="Meta icon"/>
-              <p className='socials'>Meta</p>
+              <img className="social-link-image" src={Meta} alt="Meta icon"/>
+              <p className="social-link-text">Meta</p>
             </a>
           </div>
       </div> 
