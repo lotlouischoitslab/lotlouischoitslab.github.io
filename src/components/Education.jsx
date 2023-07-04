f5c260a63ce62fb5dd1d75b85845a8157a307a17
lotlouischoitslab <louis.s.cho@gmail.com>
2023-07-04 02:20:39 -0500

    add

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,31 +1,34 @@
-import React from 'react'
-import './css/Education.css'
+import React from 'react';
+import './css/Education.css';
+import img1 from '../images/uiuc.png';
+
 
 class Education extends React.Component {
-  render (){
-  return (
-    <div id='education' name='education' className='education'>
-      <div className='education-title'>
-        EDUCATION
-      </div>
-      <div className ="university-title">
-            University of Illinois at Urbana-Champaign <span className = "title-colored"></span>
-          </div>
-          <div className = "education-date">
-            August 2020 - May 2024 (Expected)
-          </div>
-          <ul class='education-description'>
-            <li className='elem'>
-              Pursuing a B.S in Civil & Environmental Engineering (Transportation)
-            </li>
-            <li className='elem'>
-              Pursuing a Minor in Computer Science (Artificial Intelligence & Big Data)
-            </li>
+  render() {
+    return (
+      <div id="education" name="education" className="education">
+        <div className="education-title">EDUCATION</div>
 
-          </ul> 
-      
-    </div>
-  )
- }
+        <div className="education-box">
+          <div className="education">
+            <div className="education-content">
+              <div className="image-container">
+                <img src={img1} alt="Education 1" className="education-image" />
+              </div>
+              <div className="education-description">
+                <ul>
+                  <li className="place-title">University of Illinois at Urbana-Champaign (UIUC)</li>
+                  <li >August 2020 - May 2024 (Expected)</li>
+                  <li>Pursuing a B.S in Civil & Environmental Engineering (Transportation)</li>
+                  <li>Pursuing a Minor in Computer Science (Artificial Intelligence & Big Data)</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-export default Education
+
+export default Education;
---
+++ src/components/ParticlesBackground.js
@@ -0,0 +1,12 @@
+import Particles from 'react-tsparticles';
+import particlesConfig from './config/particles-config';
+
+const ParticleBackground = () => {
+    return (
+        <Particles>
+            
+        </Particles>
+    )
+}
+
+export default ParticleBackground;
