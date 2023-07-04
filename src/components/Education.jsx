798e4c7a6a2b9c22aba83c7828b4b813b3f24f4d
lotlouischoitslab <louis.s.cho@gmail.com>
2023-07-04 02:46:36 -0500

    add

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -12,7 +12,7 @@ class Education extends React.Component {
         <div className="education-box">
           <div className="education">
             <div className="education-content">
-              <div className="image-container">
+              <div className="eimage-container">
                 <img src={img1} alt="Education 1" className="education-image" />
               </div>
               <div className="education-description">
