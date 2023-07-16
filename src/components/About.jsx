ff281ecf2079d7a077fc0181ce132cebfddd13cb
lotlouischoitslab <louis.s.cho@gmail.com>
2023-07-16 16:30:32 -0500

    add

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,17 +9,20 @@ class About extends React.Component {
                 <div className="about-title">
                     ABOUT ME
                 </div>
-                <div className="about-grid">
+                <div className="about-box">
                     <div className="about-bio">
-                        I was born in Boston, Massachusetts and lived in the U.S for 9 years before moving to South Korea where I lived in Seoul for 11 years. I moved back to the U.S and I am currently living in Urbana, Illinois.
-                        I have always been fascinated about transportation and enjoy traveling by airplanes, trains, buses and cars.
-                        With AI and IoT enhancing our mobility, my interests in Intelligent Transportation Systems (ITS) are Motion Planning and Decision-Making
-                        Algorithms using Deep Reinforcement Learning and Computer Vision for Autonomous Vehicles. I am fluent in both English and Korean.
-                        My hobbies are listening and singing Korean pop and rap songs, eating Korean food, and watching Korean movies. I also love playing around with ChatGPT.
-                        I have further plans going to graduate school to pursue my dream of becoming a Computational Transportation Scientist.
+                        I was born in Boston, Massachusetts, and spent 9 years living in the United States before moving to Seoul, South Korea, 
+                        where I lived for 11 years. I returned to the U.S and currently live in Urbana, Illinois. My fascination with 
+                        transportation has been a lifelong passion, and I enjoy traveling by airplanes, trains, buses, and cars. With the 
+                        advancements in AI and IoT, I am particularly interested in Intelligent Transportation Systems (ITS) and specialize in Motion 
+                        Planning and Decision-Making Algorithms utilizing Deep Reinforcement Learning and Computer Vision for Autonomous Vehicles. 
+                        Being fluent in both English and Korean allows me to navigate and communicate effectively in diverse settings. In my free time, 
+                        I listen and sing Korean pop and rap songs, taste the flavors of Korean cuisine, and watch Korean movies and dramas. I also 
+                        enjoy exploring the capabilities of ChatGPT. Looking ahead, I have ambitious plans to pursue graduate studies in order to
+                        fulfill my dream of becoming a Computational Transportation Scientist, propelling me further in this exciting field.
                     </div>
-                    <div className='about-img'>
-                        <img src={Louis}/>
+                    <div className='about-img-container'>
+                        <img src={Louis} className='about-image'/>
                     </div>
                 </div>
             </div>
