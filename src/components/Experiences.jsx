ed14db46950998659c7cbdcd1480ecbca20d00a8
lotlouischoitslab <louis.s.cho@gmail.com>
2024-02-17 19:12:27 -0600

    add

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -70,7 +70,8 @@ class Experiences extends React.Component {
                   professionals in the transportation industry and academia. ITE@UIUC hosts two major Transportation Student-Professional Networking 
                   Panels throughout the school year. Every Fall, guest speakers from the Illinois Traffic Engineering Safety Conference (TES) would 
                   join our panel to discuss the modern advances in Smart Mobility. Every Spring, guest speakers from the Illinois Transportation and 
-                  Highway Engineering Conference (T.H.E) would join our panel to discuss about roads and highways. Additionally, I lead the Transportation Data Science Team of 10 students to analyze transit data using various Machine Learning algorithms and Deep Learning techniques 
+                  Highway Engineering Conference (T.H.E) would join our panel to discuss about roads and highways. Additionally, I lead the 
+                  Transportation Data Science Team of 6 students to analyze transit data using various Machine Learning algorithms and Deep Learning techniques 
                   for classification tasks. For all the ITE@UIUC projects, I take a crucial role at the UIUC Engineering Open House (EOH) to proudly 
                   present all of them. Not only this, but I also mentor passionate Civil Engineering (Transportation) majors, and Computer Science 
                   majors and minors by offering thorough curriculum planning guidance and coursework selection. I am also a proud member representing 
