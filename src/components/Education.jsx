25fab239d04edffa4e977f14adc5e50b22ad5e43
lotlouischoitslab <louis.s.cho@gmail.com>
2024-06-19 18:59:48 +0900

    add

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -8,25 +8,24 @@ class Education extends React.Component {
     return (
       <div id="education" name="education" className="education">
         <div className="education-title">EDUCATION</div>
-
-        <div className="education-box">
-          <div className="education">
-            <div className="education-content">
-              <div className="eimage-container">
-                <img src={img1} alt="Education 1" className="education-image" />
-              </div>
-              <div className="education-description">
-                <ul>
-                  <li className="place-title">University of Illinois at Urbana-Champaign (UIUC)</li>
-                  <li>August 2024 - May 2025 (Expected)</li>
-                  <li>M.S in Civil Engineering (Transportation)</li>
-                  <li>August 2020 - May 2024 (Expected)</li>
-                  <li>B.S in Civil Engineering (Transportation)</li>
-                  <li>Minor in Computer Science</li>
-                </ul>
+          <div className="education-box">
+            <div className="education">
+              <div className="education-content">
+                <div className="eimage-container">
+                  <img src={img1} alt="Education 1" className="education-image" />
+                </div>
+                <div className="education-description">
+                  <ul>
+                    <li className="place-title">University of Illinois at Urbana-Champaign (UIUC)</li>
+                    <li>August 2024 - May 2025 (Expected)</li>
+                    <li>M.S in Civil Engineering (Transportation)</li>
+                    <li>August 2020 - May 2024</li>
+                    <li>B.S in Civil Engineering (Transportation)</li>
+                    <li>Minor in Computer Science</li>
+                  </ul>
+                </div>
               </div>
             </div>
-          </div>
         </div>
       </div>
     );
