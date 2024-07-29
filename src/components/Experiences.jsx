6773f53c40968ed9fe883bcd923f5dec69f508c5
lotlouischoitslab <louis.s.cho@gmail.com>
2024-07-29 18:57:44 -0500

    add

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './css/Experiences.css';
+import img6 from '../images/autonomy_transportation.png'
 import img5 from '../images/av_trajectory.png';
 import img4 from '../images/GPT.png';
 import img3 from '../images/ite_uiuc.png';
@@ -10,27 +11,47 @@ class Experiences extends React.Component {
   render() {
     return (
       <div id="experiences" name="experiences" className="experiences">
-        <div className="experiences-title">EXPERIENCES</div>
-
-        <div className="experience-box">
-          <div className="experience">
-            <div className="job-title">
-              Undergraduate Researcher @ Smart City Laboratory, UIUC
-            </div>
-
-            <div className="job-date">August 2023 - Present</div>
+        <div className="experiences-title">EXPERIENCES</div> 
+          <div className="experience-box">
+            <div className="experience">
+              <div className="job-title">
+                Graduate Research Assistant @ Smart City Laboratory, UIUC
+              </div>
+              <div className="job-date">August 2022 - May 2024</div>
               <div className="experience-content">
                 <div className="image-container">
-                  <img src={img5} alt="Experience 1" className="experience-image" />
+                  <img src={img6} alt="Experience 1" className="experience-image" />
                 </div>
                 <div className="job-description">
                   <p>
-                  I am actively collaborating with Professor Alireza Talebpour in the Department of Civil & Environmental Engineering (Transportation) 
-                  at UIUC. My role is to develop an efficient probabilistic trajectory prediction algorithm for Autonomous Vehicles by integrating AI 
-                  into Traffic Flow Theory. To achieve this objective, I am leveraging LSTM neural networks to predict trajectories to safely guide 
-                  self-driving vehicles on complex traffic environment dynamics, especially when passing underneath overpasses. Through this, I aim to 
-                  safely guide autonomous vehicles when changing lanes in freeways.
-                
+                  I am actively working for Professor Alireza Talebpour in the Department of Civil & Environmental Engineering 
+                  (Transportation) at UIUC for my M.S Degree in Civil Engineering (Transportation). My plan is to do research using Artificial 
+                  Intelligence for Transportation. 
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+        <div className="experience-box"> 
+          <div className="experience">
+            <div className="job-title">
+              Undergraduate Research Assistant @ Smart City Laboratory, UIUC
+            </div>
+
+            <div className="job-date">August 2023 - May 2024</div>
+              <div className="experience-content">
+                <div className="image-container">
+                  <img src={img5} alt="Experience 2" className="experience-image" />
+                </div>
+                <div className="job-description">
+                  <p>
+                  I collaborated with Professor Alireza Talebpour in the Department of Civil & Environmental Engineering (Transportation) 
+                  at UIUC. My role was to develop a safe probabilistic vehicular trajectory prediction algorithm for Autonomous Vehicles by 
+                  integrating Artificial Intelligence into fundamental concepts in Traffic Flow Theory. I used LSTM neural networks to predict 
+                  vehicular trajectories to safely guide self-driving vehicles in complex traffic environment dynamics, especially when passing 
+                  underneath overpasses.  
                   </p>
                 </div>
               </div>
@@ -38,11 +59,11 @@ class Experiences extends React.Component {
             <div className="job-date">May 2023 - August 2023</div>
               <div className="experience-content">
                 <div className="image-container">
-                  <img src={img4} alt="Experience 1" className="experience-image" />
+                  <img src={img4} alt="Experience 3" className="experience-image" />
                 </div>
                 <div className="job-description">
                   <p>
-                  I worked closely with Professor Alireza Talebpour in the Department of Civil & Environmental Engineering (Transportation) 
+                  I worked with Professor Alireza Talebpour in the Department of Civil & Environmental Engineering (Transportation) 
                   at UIUC. The objective of this project was to leverage GPT and Deep Reinforcement Learning for Traffic State Spacing. My 
                   primary task was to create a sequence-based simulation using LSTM neural networks enabling the agent to learn the state 
                   patterns. After the agent learns the state-action-state patterns by simulating the Markov Decision Process, the aim was to 
@@ -61,7 +82,7 @@ class Experiences extends React.Component {
             <div className="job-date">August 2022 - May 2024</div>
             <div className="experience-content">
               <div className="image-container">
-                <img src={img3} alt="Experience 2" className="experience-image" />
+                <img src={img3} alt="Experience 4" className="experience-image" />
               </div>
               <div className="job-description">
                 <p>
@@ -90,7 +111,7 @@ class Experiences extends React.Component {
             <div className="job-date">January 2023 - May 2023</div>
             <div className="experience-content">
               <div className="image-container">
-                <img src={img2} alt="Experience 3" className="experience-image" />
+                <img src={img2} alt="Experience 5" className="experience-image" />
               </div>
               <div className="job-description">
                 <p>
@@ -111,7 +132,7 @@ class Experiences extends React.Component {
             <div className="job-date">May 2022 - August 2022</div>
             <div className="experience-content">
               <div className="image-container">
-                <img src={img1} alt="Experience 4" className="experience-image" />
+                <img src={img1} alt="Experience 6" className="experience-image" />
               </div>
               <div className="job-description">
                 <p>
