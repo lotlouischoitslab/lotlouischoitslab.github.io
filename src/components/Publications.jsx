68987367eb31004ff44c1c02e8c8755ae17beaaf
lotlouischoitslab <louis.s.cho@gmail.com>
2025-09-16 13:37:11 -0500

    add

--- src/components/Publications.jsx
+++ src/components/Publications.jsx
@@ -7,24 +7,17 @@ class Publications extends React.Component {
   render() {
     return (
       <div id="publications" name="publications" className="publications">
-        <div className="publications-title">PUBLICATIONS</div>
+        <div className="publications-title">PUBLICATIONS & MANUSCRIPTS</div>
           <div className="publications-box">
-            <div className="publications">
-              <div className="publications-content"> 
-                {/* <div className="image-container">
-                  <img src={img1} alt="Publication 1" className="publications-image" />
-                </div> */}
-                 <div className="publications-description">
-                   <ul>  
-                    <a href="">
-                        <li className="place-title"> Coming Soon! Stay Tuned! </li> 
-                    </a>
-                    {/* <li>Louis Sungwoo Cho, Department of Civil and Environmental Engineering (Transportation) at UIUC</li>
-                    <li>Alireza Talebpour, Department of Civil and Environmental Engineering (Transportation) at UIUC</li>
-                    <li>Transportation Research Part C: Emerging Technologies (2024) (Expected)</li>   */}
-                  </ul>
-                </div> 
-              </div>
+            <div className="publications"> 
+                <ul>  
+                <a href="">
+                    <li className="place-title"> 
+                      Cho, L. S., & Talebpour, A. (2025). Evaluating Control Spacing Strategies for Autonomous Vehicles.<br/>Under review in the ASCE Journal of Transportation Engineering, Part A: Systems. </li> 
+                    
+                </a> 
+              </ul>
+             
             </div>
         </div>
       </div>
